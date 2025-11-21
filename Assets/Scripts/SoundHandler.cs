02e286190c4d5ca499b3ee7c8188356fe82bca3d
Porky <sixten.ljungstrom@gmail.com>
2025-11-21 14:11:30 +0100

    Looping 2d music

---
+++ Assets/Scripts/SoundHandler.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundHandler : MonoBehaviour
+{
+    public static SoundHandler Instance { get; private set; }
+    private List<AudioSource> sources = new List<AudioSource>();
+
+    [SerializeField] private AudioSource _sourcePrefab;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = sources.Count - 1; i >= 0; i--)
+        {
+            if (!sources[i].isPlaying)
+            {
+                Destroy(sources[i].gameObject);
+                sources.RemoveAt(i);
+            }
+        }
+    }
+
+    public void PlaySound(AudioClip clip, Vector2 pos, bool loop = false)
+    {
+        AudioSource source = Instantiate(_sourcePrefab);
+        source.transform.position = pos;
+
+        source.loop = loop;
+        source.clip = clip;
+        source.Play();
+        sources.Add(source);
+    }
+}
